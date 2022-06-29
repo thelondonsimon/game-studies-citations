d08b5276e3e9260ec8244ef53e5bee6f0be77d85
Simon Chambers <simontchambers@gmail.com>
2022-06-30 09:50:00 +1000

    add comments to preliminary-analysis

--- analysis/preliminary-analysis.R
+++ analysis/preliminary-analysis.R
@@ -1,15 +1,23 @@
-library(tidyverse)
-library(tidygraph)
-library(ggraph)
-library(readxl)
-library(scales)
-library(ggthemes)
-library(igraph)
-library(RColorBrewer)
-library(intergraph)
-library(ergm.count)
+library(tidyverse) # data manipulation
+library(readxl) # data loading
 
-### LOAD DATA AND REMOVE DUPLICATES
+
+library(tidygraph) # network mainpulation
+library(igraph) # network analysis
+library(ggraph) # network plotting
+library(intergraph) # converting network objects
+library(ergm) # network modelling
+
+library(scales) # scales for plots
+library(RColorBrewer) # colour management
+library(ggthemes) # ggplot themes
+
+
+
+###################################
+## LOAD DATA AND REMOVE DUPLICATES
+###################################
+
 # authors = academics with profiles on google scholar
 authors <- read_excel('../data/consolidated-data.xlsx',4) %>%
   group_by(scholar_id) %>%
@@ -119,20 +127,26 @@ manual_authors <- read_csv('manual-name-scholar-ids.csv')
 
 
 
-###############
-###############
-
-# colours for gender plots
+###############################
+## CONFIGURE COLOURS FOR PLOTS
+###############################
 reds <- brewer.pal(3,'Reds')
 blues <- brewer.pal(3,'Blues')
 purples <- brewer.pal(3,'Purples')
 
-# Get a subset of Game Studies authors based on having that tag in their profile
+
+
+################################
+## SUBSET DATA FOR GAME STUDIES
+################################
+# Get a subset of Game Studies authors based on being in the list of manually compiled authors
+# or having that tag in their profile
 gameStudiesAuthors <- authors %>%
-  filter(scholar_id %in% manual_authors$scholar_id | scholar_id %in% (author_tags %>% filter(str_to_lower(tag) == 'game studies'))$scholar_id)
+  filter(scholar_id %in% manual_authors$scholar_id |
+           scholar_id %in% (author_tags %>% filter(str_to_lower(tag) == 'game studies'))$scholar_id)
 
 
-# Get a subset of Game Studies articles based on keywords in publication titles
+# Get a subset of Game Studies publications based on keywords in publication titles
 # @todo: validate this approach
 gameArticles <- articles %>%
   filter(str_detect(title,".*gam[ei]*") |
@@ -144,7 +158,7 @@ gameArticles <- articles %>%
   )
 
 
-### OVERALL GENDER SPLIT OF GAME STUDIES SCHOLARS ###
+# Get a subset of article_author data reflecting authors of those publications in the gameArticles data
 gameArticleAuth <- article_author %>%
   filter(cite_id %in% gameArticles$cite_id) %>%
   inner_join(authors) %>%
@@ -152,32 +166,13 @@ gameArticleAuth <- article_author %>%
   select(cite_id,order,scholar_id,gender,citedby,citedby5y,country,continent,pub_year,num_citations) %>%
   rename(pub_citations = num_citations)
 
-authorCount <- gameArticleAuth %>% group_by(cite_id) %>% summarise(authorCount = n())
 
 
-geoData <- gameArticleAuth %>%
-  filter(order == 1) %>%
-  group_by(country) %>%
-  filter(is.na(country) == FALSE) %>%
-  summarise(citations = sum(pub_citations, na.rm = TRUE))
+##########
+## GENDER
+##########
 
-top <- geoData %>% slice_max(citations,n = 10)
-geoData <- geoData %>%
-  mutate(country = ifelse(country %in% top$country,country,'Other')) %>%
-  group_by(country) %>%
-  summarise(citations = sum(citations,na.rm = TRUE))
-
-ggplot(geoData,aes(fct_reorder(str_wrap(country,12),citations,.desc = TRUE),citations)) +
-  geom_col(fill = blues[3]) +
-  scale_y_continuous(labels = comma_format()) +
-  theme_hc() +
-  labs(
-    title = 'Number of Game Studies Citations, by Country of First-Author',
-    y = 'Citations',
-    x = NULL
-  )
-  
-
+# get various counts of gender split
 genderData <- gameStudiesAuthors %>%
   count(gender) %>%
   filter(!is.na(gender)) %>%
@@ -222,6 +217,7 @@ genderData <- gameStudiesAuthors %>%
                                        'Number of First\nAuthor Citations')))
 
 
+# plot splits
 ggplot(genderData,aes(type,pct,fill=gender,label = scales::percent(pct, accuracy = 1))) +
   geom_col() +
   geom_text(position = position_stack(vjust = 0.5), colour = 'white') +
@@ -236,10 +232,12 @@ ggplot(genderData,aes(type,pct,fill=gender,label = scales::percent(pct, accuracy
   )
 
 
-### HIGH CITATION
+
+
+# Gender of 100 most cited game studies authors
 citationData <- gameArticleAuth %>%
   group_by(scholar_id,gender) %>%
-  summarise(citations = sum(pub_citations)) %>%
+  summarise(citations = sum(pub_citations, na.rm = TRUE)) %>%
   filter(citations > 500) %>%
   mutate(gender = ifelse(is.na(gender),'Unspecified',gender)) %>%
   top_n(100,desc(citations))
@@ -303,21 +301,28 @@ genderData2 %>% filter(gender == 'Female' & year >= 2001 & year <= 2021) %>%
   )
 
 
-# GAME STUDIES SCHOLARS
+########
+## TAGS
+########
+# get tags of game studies authors
 gameStudyAuthorTags <- author_tags %>%
   filter(scholar_id %in% (author_tags %>% filter(codedTag == 'Game Studies'))$scholar_id) %>%
   left_join(authors) %>%
   select(scholar_id,tag,gender,codedTag)
 
-
+# remove game studies, as we know this is a common shared tag
 data <- gameStudyAuthorTags %>%
   filter(codedTag != 'Game Studies')
+
+# get overall counts and filter to those with at least 11 
 overallCounts <- data %>%
   count(codedTag) %>%
   filter(n >= 11) %>%
   mutate(tag2 = str_wrap(codedTag,8)) %>%
   mutate(pctOverall = n/nrow(gameStudiesAuthors)) %>%
   arrange(desc(n))
+
+# split the data for each tag based on gender
 data <- data %>%
   filter(codedTag %in% overallCounts$codedTag) %>%
   group_by(codedTag,gender) %>%
@@ -327,6 +332,7 @@ data <- data %>%
   left_join(overallCounts, by = 'codedTag') %>%
   mutate(tag2 = factor(tag2,levels = overallCounts$tag2))
 
+# plot dat
 ggplot(data,aes(tag2,n2,fill=gender)) + 
   geom_col() +
   geom_text(
@@ -344,22 +350,29 @@ ggplot(data,aes(tag2,n2,fill=gender)) +
   )
 
 
-
+###########################################
 ## TOP 30 most cited game studies scholars
+###########################################
 top30ScholarIds <- gameArticleAuth %>%
   group_by(scholar_id) %>%
-  summarise(citations = sum(pub_citations)) %>%
+  summarise(citations = sum(pub_citations, na.rm = TRUE)) %>%
   slice_max(citations,n=30)
 
-top30CiteIds <- gameArticleAuth %>% filter(scholar_id %in% top30ScholarIds$scholar_id) %>% select(cite_id) %>% distinct()
+# get a list of cite_ids featuring at least one of the top 30 scholars
+top30CiteIds <- gameArticleAuth %>%
+  filter(scholar_id %in% top30ScholarIds$scholar_id) %>% select(cite_id) %>%
+  distinct()
 
+# get all game studies author_article records who have co-authored with the top 30
 gameArticleAuthByTop30 <- gameArticleAuth %>%
   filter(cite_id %in% top30CiteIds$cite_id)
 
+# data for top 30 authors
 top30Data <- top30ScholarIds %>%
   inner_join(authors) %>%
   select(scholar_id,citations,name,gender,continent)
 
+# plot
 top30Data %>% ggplot(aes(fct_reorder(name,citations,.desc = TRUE),citations,fill=gender)) +
   geom_col() +
   scale_y_continuous(labels = comma_format()) +
@@ -372,7 +385,45 @@ top30Data %>% ggplot(aes(fct_reorder(name,citations,.desc = TRUE),citations,fill
     x = NULL
   )
 
+
+
+
+#############
+## GEOGRAPHY
+#############
+# get the total number of first-author citations, by country
+geoData <- gameArticleAuth %>%
+  filter(order == 1) %>%
+  group_by(country) %>%
+  filter(is.na(country) == FALSE) %>%
+  summarise(citations = sum(pub_citations, na.rm = TRUE))
+
+# get the top 10 countries, and recorde all other countries as 'Other'
+top <- geoData %>% slice_max(citations,n = 10)
+geoData <- geoData %>%
+  mutate(country = ifelse(country %in% top$country,country,'Other')) %>%
+  group_by(country) %>%
+  summarise(citations = sum(citations,na.rm = TRUE))
+
+# plot data
+ggplot(geoData,aes(fct_reorder(str_wrap(country,12),citations,.desc = TRUE),citations)) +
+  geom_col(fill = blues[3]) +
+  scale_y_continuous(labels = comma_format()) +
+  theme_hc() +
+  labs(
+    title = 'Number of Game Studies Citations, by Country of First-Author',
+    y = 'Citations',
+    x = NULL
+  )
+
+
+
+###############################################################
+## NETWORK ANALYSIS OF CO-OCCURING GOOGLE SCHOLAR PROFILE TAGS
+###############################################################
+
 # create a bipartite network of scholars and tags
+# assemble a single set of vertices representing either authors or tags
 vAuthors <- gameArticleAuth %>%
   filter(order == 1) %>%
   select(scholar_id,gender,citedby,citedby5y,country,continent) %>%
@@ -392,6 +443,7 @@ vTags <- author_tags %>%
 vertices = vAuthors %>%
   bind_rows(vTags)
 
+# assemble edges between authors and tags of first-authored game studies articles
 edges <- gameArticles %>%
   inner_join(article_author) %>%
   filter(order == 1) %>%
@@ -402,9 +454,13 @@ edges <- gameArticles %>%
   rename(from = id.x, to = id.y) %>%
   select(from,to)
 
+# create graph
+# @note: there is an issue with including the `name` field in the call to tbl_graph, so appending it later
 g <- tbl_graph(nodes = vertices %>% select(id,gender,type), edges = edges, directed = FALSE) %>%
   activate(nodes) %>%
   mutate(name = vertices$name)
+
+# extract the projection of the tag network and filter based on edge weight and node degree
 graphs <- bipartite.projection(g)
 tagGraph <- as_tbl_graph(graphs$proj2) %>%
   filter(name != 'Game Studies') %>%
@@ -417,6 +473,7 @@ weights <- E(tagGraph)$weight
 tagGraph <- tagGraph %>%
   mutate(cluster = group_louvain(weights=weights))
 
+# plot the graph
 tagGraph %>%
   ggraph() + 
   geom_edge_link(
@@ -441,7 +498,16 @@ tagGraph %>%
   
 
 
-# co-authorship network
+#####################################
+## NETWORK ANALYSIS OF CO-AUTHORSHIP
+#####################################
+
+# base this network on the co-authorship among game studies articles whose
+# list of authors includes at least one of the top 30 most cited authors
+
+# construct a bipartite network of publciation-author relationships similar to
+# the approach above for the author-tag network
+
 vAuthors <- authors %>%
   select(scholar_id,name,gender,citedby,citedby5y,country,continent) %>%
   filter(scholar_id %in% gameArticleAuthByTop30$scholar_id) %>%
@@ -503,11 +569,20 @@ authorGraph %>%
   theme_graph()
 
 
+
+#################
+## MODEL NETWORK
+#################
+
+# convert igraph to statnet network
 coAuthorNetwork <- asNetwork(authorGraph)
+
+# get baseline network based on edge count only
 coAuthor.models.baseline <- ergm(coAuthorNetwork ~ edges)
 summary(coAuthor.models.baseline)
+
+# model based on node attributes only
 coAuthor.models.1 <- ergm(coAuthorNetwork ~ edges +
-                          gwesp(log(2),fixed=TRUE) +
                           nodefactor('gender') +
                           nodematch('gender') +
                           nodecov('citedby5y') +
